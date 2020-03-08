c3d7dfe8ec813e01d7db73594e4a5671f0e010ad
Kieran Flaherty <kieranflaherty1995@gmail.com>
2020-03-08 23:43:03 +0000

    feat: Add function to retrieve ingredient data from player inventory
    * Add hasValue to helpers to check for value in table
    * Add tableString to helpers for logging table data
    * Add getInventoryIngredients to main to retrieve player ingredient data from inventory

--- helpers.lua
+++ helpers.lua
@@ -2,8 +2,31 @@ local Helpers = {}
 
 function Helpers.log(data, funcName)
     funcName = funcName or ""
+    data = data or ""
     local logString = string.format("[alchemy][%s] %s", funcName, data)
     print(logString)
+end
+
+function Helpers.hasValue(tab, value)
+    for _, val in ipairs(tab) do
+        if val == value then return true end
+    end
+    return false
+end
+
+-- TODO: Improve this with indentation
+function Helpers.tableString(tab)
+    if type(tab) == 'table' then
+        local s = '{\n'
+        for k,v in pairs(tab) do
+            if type(k) ~= 'number' then k = '"'..k..'"' end
+            if type(v) == 'table' then v = Helpers.tableString(v) end
+            s = s .. '['..k..'] = ' .. v .. ','
+        end
+        return s .. '\n} '
+    else
+        return ""
+    end
 end
 
 return Helpers
--- main.lua
+++ main.lua
@@ -8,4 +8,25 @@ function init(e)
     end
 end
 
+function getInventoryIngredients()
+    helpers.log("Getting inventory ingredients...", "getInventoryIngredients")
+    local ingredientsInInventory = {}
+    for itemStack in tes3.iterate(tes3.player.object.inventory.iterator) do
+        local count = itemStack.count
+        if helpers.hasValue(ingredients, tostring(itemStack.object)) then
+            helpers.log(string.format("Found ingredient: %s, count: %d", itemStack.object, count),
+                "getInventoryIngredients")
+            table.insert(ingredientsInInventory, {item = tostring(itemStack.object), count = count})
+        end
+    end
+    return ingredientsInInventory
+end
+
+function onCommand()
+    helpers.log(nil, "onCommand")
+    local availableIngredients = getInventoryIngredients()
+    helpers.log(string.format("Avaliable ingredients: %s", helpers.tableString(availableIngredients)), "onCommand")
+end
+
 event.register("initialized", init)
+event.register("keyDown", onCommand, { filter = tes3.scanCode.pipe })
